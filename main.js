caa4b2a5901e48f435e2d2a7ee3155b347cddb0e
Tarcio2020 <94722456+Tarcio2020@users.noreply.github.com>
2023-05-27 01:48:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,46 @@ $('.menu-hamburguer').click(function(){ //ao clicar no menu hamburguer teremos o
 })
 
 
+$('#telefone').mask('(00) 00000-0000',{
+    placeholder: '(00) 00000-0000'
+})
+    
+//$('#telefone').mask('00/00/0000', {
+    
+//  placeholder: '__/__/____'
+$('form').validate({
+    rules: {
+        nome: {
+            required: true
+            },
+        email: {
+            required: true,
+            email: true
+        },
+        telefone: {
+            required: true
+        },
+        menssagem: {
+            required: true,
+        },
+        veiculoDeinteresse: {
+            required: false
+        }
+
+    },
+    menssagens: {
+        nome: 'Por favor, insira o seu nome'
+    },
+    submitHandler: function(form) {
+        console.log(form)
+    },
+    invalidHandler: function(evento, validador) {
+        let camposIncorretos = validador.numberOfInvalids();
+        if(camposIncorretos){
+            alert(`Existem ${camposIncorretos} campos incorretos`)
+        }
+    }
+})
+
+
 });
